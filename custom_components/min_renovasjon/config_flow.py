2a38b01fe02088116efa52942f36d361e79dc3bf
Eyesoft <audun.simonsen@gmail.com>
2022-09-07 10:16:07 +0200

    Update config_flow.py

--- custom_components/min_renovasjon/config_flow.py
+++ custom_components/min_renovasjon/config_flow.py
@@ -4,3 +4,11 @@ from .const import DOMAIN
 
 class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Example config flow."""
+    async def async_step_user(self, info):
+        if info is not None:
+            pass  # TODO: process info
+
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema({vol.Required("password"): str})
+        )
+
