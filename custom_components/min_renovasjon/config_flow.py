72ac4d58ef0a5f1af26d33dcf56e94fbafd7ad31
Eyesoft <audun.simonsen@gmail.com>
2022-09-09 15:25:12 +0200

    Add files via upload

--- custom_components/min_renovasjon/config_flow.py
+++ custom_components/min_renovasjon/config_flow.py
@@ -1,118 +1,223 @@
-"""Config flow for Hello World integration."""
-from __future__ import annotations
+import aiohttp
+import asyncio
+import json
+
+from collections import namedtuple
+from typing import Dict, List, Tuple
+import requests
+import re
 
 import logging
-from typing import Any
-
 import voluptuous as vol
 
 from homeassistant import config_entries, exceptions
 from homeassistant.core import HomeAssistant
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
 
-from .const import DOMAIN  # pylint:disable=unused-import
-from .hub import Hub
+from .const import (
+    DOMAIN,
+    CONF_STREET_NAME,
+    CONF_STREET_CODE,
+    CONF_HOUSE_NO,
+    CONF_COUNTY_ID,
+    CONF_DATE_FORMAT,
+    DEFAULT_DATE_FORMAT,
+    CONF_FRACTION_IDS,
+    ADDRESS_LOOKUP_URL,
+    APP_CUSTOMERS_URL,
+    CONST_APP_KEY_VALUE,
+    KOMTEK_API_BASE_URL,
+    CONST_URL_FRAKSJONER
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-# This is the schema that used to display the UI to the user. This simple
-# schema has a single required host field, but it could include a number of fields
-# such as username, password etc. See other components in the HA core code for
-# further examples.
-# Note the input displayed to the user will be translated. See the
-# translations/<lang>.json file and strings.json. See here for further information:
-# https://developers.home-assistant.io/docs/config_entries_config_flow_handler/#translations
-# At the time of writing I found the translations created by the scaffold didn't
-# quite work as documented and always gave me the "Lokalise key references" string
-# (in square brackets), rather than the actual translated value. I did not attempt to
-# figure this out or look further into it.
-DATA_SCHEMA = vol.Schema({("host"): str})
-
-
-async def validate_input(hass: HomeAssistant, data: dict) -> dict[str, Any]:
-    """Validate the user input allows us to connect.
-
-    Data has the keys from DATA_SCHEMA with values provided by the user.
-    """
-    # Validate the data can be used to set up a connection.
-
-    # This is a simple example to show an error in the UI for a short hostname
-    # The exceptions are defined at the end of this file, and are used in the
-    # `async_step_user` method below.
-    if len(data["host"]) < 3:
-        raise InvalidHost
-
-    hub = Hub(hass, data["host"])
-    # The dummy hub provides a `test_connection` method to ensure it's working
-    # as expected
-    result = await hub.test_connection()
-    if not result:
-        # If there is an error, raise an exception to notify HA that there was a
-        # problem. The UI will also show there was a problem
-        raise CannotConnect
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
-    # Return info that you want to store in the config entry.
-    # "Title" is what is displayed to the user for this hub device
-    # It is stored internally in HA as part of the device config.
-    # See `async_step_user` below for how this is used
-    return {"title": data["host"]}
-
-
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Hello World."""
 
     VERSION = 1
-    # Pick one of the available connection classes in homeassistant/config_entries.py
-    # This tells HA if it should be asking for updates, or it'll be notified of updates
-    # automatically. This example uses PUSH, as the dummy hub will notify HA of
-    # changes.
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
-
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+    
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        # This goes through the steps to take the user through the setup process.
-        # Using this it is possible to update the UI and prompt for additional
-        # information. This example provides a single form (built from `DATA_SCHEMA`),
-        # and when that has some validated input, it calls `async_create_entry` to
-        # actually create the HA config entry. Note the "title" value is returned by
-        # `validate_input` above.
         errors = {}
+        address = None
+        
         if user_input is not None:
             try:
-                info = await validate_input(self.hass, user_input)
-
-                return self.async_create_entry(title=info["title"], data=user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidHost:
-                # The error string is set here, and should be translated.
-                # This example does not currently cover translations, see the
-                # comments on `DATA_SCHEMA` for further details.
-                # Set the error on the `host` field, not the entire form.
-                errors["host"] = "cannot_connect"
-            except Exception:  # pylint: disable=broad-except
+                address = user_input["address"]
+                error, address_info = await self._get_address_info(address)
+                
+                if error is not None:
+                    errors["base"] = error
+                
+                if address_info is not None:
+                    return self.async_create_entry(title="Min Renovasjon", data=address_info)
+                    
+            except Exception:
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
 
-        # If there is no user input or there were errors, show the form again, including any errors that were found with the input.
+        #errors substitution in language file
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+            step_id="user",
+
+            data_schema=vol.Schema({
+            	vol.Required("address", default=address): str
+            }),
+            errors=errors
         )
 
+    async def _get_address_info(self, address_search_string):
+        error, address_info = await self._address_lookup(address_search_string)
 
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
+        if error is not None:
+            return error, None
 
+        if address_info is not None:
+            (
+                self.street,
+                self.street_code,
+                self.number,
+                self.municipality,
+                self.municipality_code,
+                self.postal_code,
+                self.postal,
+            ) = address_info
+       
+        if await self.municipality_is_app_customer:
+            text = "self.fractions = self._get_fractions()"
+        else:
+            return "municipality_not_customer", None
+       
+        address = {
+            "street_name": self.street,
+            "street_code": str(self.street_code),
+            "house_no": str(self.number),
+            "county_id": str(self.municipality_code)
+        }        
+        
+        return None, address
 
-class InvalidHost(exceptions.HomeAssistantError):
-    """Error to indicate there is an invalid hostname."""
+    async def _address_lookup(self, s: str) -> Tuple:
+        """
+        Makes an API call to geonorge.no, the official resource for open geo data in Norway.
+        This function is used to get deterministic address properties that is needed for
+        further API calls with regards to Min Renovasjon, mainly municipality, municipality code,
+        street name and street code.
+        :param s: Search string for which address to search
+        :return: Tuple of address fields
+        """
+        error = None
+        data = None
+
+        regex = r"(.*ve)(i|g)(.*)"
+        subst = "\\1*\\3"
+        search_string = re.sub(regex, subst, s, 0, re.MULTILINE)
+
+        params={
+            "sok": search_string,
+            # Only get the relevant address fields
+            "filtrer": "adresser.kommunenummer,"
+            "adresser.adressenavn,"
+            "adresser.adressekode,"
+            "adresser.nummer,"
+            "adresser.kommunenavn,"
+            "adresser.postnummer,"
+            "adresser.poststed",
+        }
+           
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=ADDRESS_LOOKUP_URL, params=params) as resp:
+                response = await resp.read()
+                if resp.ok:
+                    data = json.loads(response.decode("UTF-8"))
+        
+        if not data["adresser"]:
+            return "no_address_found", None
+
+        if len(data["adresser"]) > 1:
+            return "multiple_addresses_found", None
+
+        return None, (
+            data["adresser"][0]["adressenavn"],
+            data["adresser"][0]["adressekode"],
+            data["adresser"][0]["nummer"],
+            data["adresser"][0]["kommunenavn"],
+            data["adresser"][0]["kommunenummer"],
+            data["adresser"][0]["postnummer"],
+            data["adresser"][0]["poststed"],
+        )
+
+    @property
+    async def municipality_is_app_customer(self) -> bool:
+        """
+        Make an API call to get all customers of the NorkartRenovasjon service which
+        supports the Min Renovasjon app. Then check if this municipality is actually
+        a customer or not.
+        :return: Boolean indicating if this municipality is a customer or not.
+        """
+        customers = None
+       
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=APP_CUSTOMERS_URL, params={"Appid": "MobilOS-NorkartRenovasjon"}) as resp:
+                response = await resp.read()
+                if resp.ok:
+                    customers = json.loads(response.decode("UTF-8"))
+        
+        return any(
+            customer["Number"] == self.municipality_code for customer in customers
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get options flow."""
+        return OptionsFlowHandler(config_entry)
+        
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Options flow handler."""
+
+    def __init__(self, config_entry):
+        """Initialize options flow."""
+        self.config_entry = config_entry
+        self.options = dict(config_entry.options)
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+
+        if user_input is not None:
+            self.options.update(user_input)
+            return self.async_create_entry(title=DOMAIN, data=self.options)
+
+        options = self.config_entry.options
+        fraction_ids = options.get(CONF_FRACTION_IDS, [])
+        
+        municipality_code = self.config_entry.data.get(CONF_COUNTY_ID, "")
+        fraction_list = await self._get_fractions(municipality_code)
+        fractions = {}
+        
+        for fraction in fraction_list:
+            fractions[str(fraction["Id"])] = fraction["Navn"]
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_FRACTION_IDS, default=fraction_ids): cv.multi_select(fractions)
+                }
+            )
+        )
+
+    async def _get_fractions(self, municipality_code) -> Dict:
+        headers = {"RenovasjonAppKey": CONST_APP_KEY_VALUE, "Kommunenr": municipality_code}
+        params = {"server": CONST_URL_FRAKSJONER}
+        
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.get(url=KOMTEK_API_BASE_URL, params=params) as resp:
+                response = await resp.read()
+                if resp.ok:
+                    return json.loads(response.decode("UTF-8"))
+
+        return None
+
